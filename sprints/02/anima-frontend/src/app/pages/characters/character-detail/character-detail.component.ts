503545a8e6e6f7dd3ae70d27b9ffa548977d4732
Mikó Sándor <sanyi0410@gmail.com>
2025-11-17 17:00:29 +0100

    Alap Angular-al generált oldalak

---
+++ sprints/02/anima-frontend/src/app/pages/characters/character-detail/character-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './character-detail.component.html',
+  styleUrl: './character-detail.component.scss'
+})
+export class CharacterDetailComponent {
+
+}
---
+++ sprints/02/anima-frontend/src/app/pages/characters/character-form/character-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './character-form.component.html',
+  styleUrl: './character-form.component.scss'
+})
+export class CharacterFormComponent {
+
+}
